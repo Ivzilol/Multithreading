57df87d8e6a7adb62fbaedf1038b3a5f2804ffa7
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-10-13 21:15:22 +0300

    Wait and Notify

---
+++ src/ReEntrantLocks/App.java
@@ -0,0 +1,33 @@
+package ReEntrantLocks;
+
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final Runner runner = new Runner();
+
+        Thread t1 = new Thread(() -> {
+            try {
+                runner.firstThread();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                runner.secondThread();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        runner.finished();
+    }
+}

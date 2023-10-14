3824e8e6408ee1b48689818b8a28807566286039
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-10-14 18:05:42 +0300

    Multithreading

---
+++ src/SwingWithSwingWorker/App.java
@@ -0,0 +1,14 @@
+package SwingWithSwingWorker;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame("Swing Work Demo");
+            }
+        });
+    }
+}

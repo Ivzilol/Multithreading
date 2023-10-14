756c23146aae0108da331482a7f44a63bc0fa7dc
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-10-14 13:57:02 +0300

    Multithreading

--- src/Semaphores/App.java
+++ src/Semaphores/App.java
@@ -1,4 +1,10 @@
 package Semaphores;
 
+import java.util.concurrent.Semaphore;
+
 public class App {
+    public static void main(String[] args) throws InterruptedException {
+
+
+    }
 }
---
+++ src/Semaphores/Connection.java
@@ -0,0 +1,6 @@
+package Semaphores;
+
+
+
+public class Connection {
+}
